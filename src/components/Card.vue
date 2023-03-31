567f3d399b790c6820beafa4b1f262bcec4e32bb
shortytapki <v.v.vinogradov99@gmail.com>
2023-04-01 01:34:05 +0300

    feat: Добавлен рендер карточек с постами
    С помощью API Hacker News клиенту приходит и
    отображается информация о последних 10 постах на
    сайте ycombinator

---
+++ src/components/Card.vue
@@ -0,0 +1,30 @@
+<script setup>
+const props = defineProps(["title", "author", "date"]);
+</script>
+
+<template>
+    <div class="card">
+        <h2>
+            <slot name="title">{{ title }}</slot>
+        </h2>
+        <p>Posted by
+            <strong>
+                <slot name="author">{{ author }}</slot>
+            </strong>
+            at <slot name="date">{{ date }}</slot>
+        </p>
+    </div>
+</template>
+<style>
+.card {
+    width: auto;
+    padding: 30px;
+    border: 3px solid black;
+    border-radius: 5px;
+    margin-bottom: 20px;
+}
+
+h2 {
+    font-size: 30px;
+    font-weight: bold;
+}</style>
--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,17 +1,32 @@
 <script setup>
-import { reactive, computed } from 'vue';
+import Card from './Card.vue';
+import { reactive, computed, watch, ref } from 'vue';
+import { getLatestPosts } from '../apiService';
 
 const data = reactive({
-  cards: ['JS', 'C', 'Java', 'PHP', 'Python'],
-  text: '',
+  cards: [],
+  text: ''
 });
+
+
+const handler = async () => {
+  data.cards = await getLatestPosts();
+};
+
 const filteredCards = computed(() => {
-  return data.cards.filter((el) => (data.text ? el.includes(data.text) : el));
+  return data.cards.filter((el) => (data.text ?
+    el.title.toLowerCase().includes(data.text.toLowerCase()) :
+    el
+  ));
 });
+
 </script>
 <template>
   <input type="text" v-model="data.text" />
-  <div v-for="card in filteredCards">
-    {{ card }}
-  </div>
+  <button @click="handler">Load new posts</button>
+  <Card v-for="card in filteredCards">
+    <template #title>{{ card.title }}</template>
+    <template #author>{{ card.by }}</template>
+    <template #date>{{ new Date(card.time * 1000).toLocaleDateString() }}</template>
+  </Card>
 </template>
