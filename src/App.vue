61eb19e78ad63d63d9d5ea03262af0c7c1497340
shortytapki <v.v.vinogradov99@gmail.com>
2023-04-07 09:51:30 +0300

    feat: Добавлен снекбар
    Добавлен снекбар с использованием
    Teleport и Transition

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <script setup>
 import Cards from './components/Cards.vue';
+import Doge from './components/Doge.vue';
+
+import { ref } from 'vue';
+
+  const dogeInDaHouse = ref(false);
+  
+  const dogeShower = () => {
+    dogeInDaHouse.value = true;
+    setTimeout(() => dogeInDaHouse.value = false, 3000);
+  }
 </script>
 
 <template>
   <Cards />
+  <Doge :dogeInDaHouse="dogeInDaHouse"/>
+  <button @click="dogeShower">Show Doge!!!</button>
 </template>
